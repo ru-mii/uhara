6d001ceb14e6ff2b6fb936f699842d7dd3cae232
ru-mii <rumiireal@gmail.com>
2025-07-15 21:05:46 +0100

    Update Main.cs

--- Uhara/Code/Interface/Main.cs
+++ Uhara/Code/Interface/Main.cs
@@ -5,7 +5,6 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
