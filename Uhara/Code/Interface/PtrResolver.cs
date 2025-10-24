3f1b37e42f09fc463bb533f59f9a6ec6b25e8819
ru-mii <rumiireal@gmail.com>
2025-10-24 02:27:39 +0100

    Refactor PtrResolver Watch and WatchString methods
    Replaced WatchFull and WatchStringFull with private _Watch and _WatchString methods to reduce code duplication and improve maintainability. Updated all overloads to use the new private methods. Added a new WatchString overload for more flexible parameter usage.

--- Uhara/Code/Interface/PtrResolver.cs
+++ Uhara/Code/Interface/PtrResolver.cs
@@ -10,20 +10,25 @@ public class PtrResolver : MainShared
 {
     public void Watch<T>(string name, object base_, params int[] offsets) where T : unmanaged
     {
-        WatchFull<T>(name, base_, offsets: offsets);
+        _Watch<T>(name, base_, offsets: offsets);
     }
 
     public void Watch<T>(string name, string moduleName, object base_, params int[] offsets) where T : unmanaged
     {
-        WatchFull<T>(name, base_, moduleName, offsets);
+        _Watch<T>(name, base_, moduleName, offsets);
     }
 
     public void Watch<T>(string name, Module module, object base_, params int[] offsets) where T : unmanaged
     {
-        WatchFull<T>(name, base_, module.Name, offsets);
+        _Watch<T>(name, base_, module.Name, offsets);
     }
 
-    public void WatchFull<T>(string name, object base_, string moduleName = null, params int[] offsets) where T : unmanaged
+    public void Watch<T>(string name, object base_, string moduleName = null, params int[] offsets) where T : unmanaged
+    {
+        _Watch<T>(name, base_, moduleName, offsets);
+    }
+
+    private void _Watch<T>(string name, object base_, string moduleName = null, params int[] offsets) where T : unmanaged
     {
         try
         {
@@ -48,65 +53,71 @@ public class PtrResolver : MainShared
 
     public void WatchString(string name, object base_, params int[] offsets)
     {
-        WatchStringFull(name, base_, offsets: offsets);
+        _WatchString(name, base_, offsets: offsets);
     }
 
     public void WatchString(string name, int length, object base_, params int[] offsets)
     {
-        WatchStringFull(name, base_, length: length, offsets: offsets);
+        _WatchString(name, base_, length: length, offsets: offsets);
     }
 
     public void WatchString(string name, ReadStringType readStringType, object base_, params int[] offsets)
     {
-        WatchString(name, base_, offsets: offsets);
+        _WatchString(name, base_, offsets: offsets);
     }
 
     public void WatchString(string name, int length, ReadStringType readStringType, object base_, params int[] offsets)
     {
-        WatchStringFull(name, base_, length, offsets: offsets);
+        _WatchString(name, base_, length, offsets: offsets);
     }
 
     public void WatchString(string name, string moduleName, object base_, params int[] offsets)
     {
-        WatchStringFull(name, base_, moduleName: moduleName, offsets: offsets);
+        _WatchString(name, base_, moduleName: moduleName, offsets: offsets);
     }
 
     public void WatchString(string name, int length, string moduleName, object base_, params int[] offsets)
     {
-        WatchStringFull(name, base_, length, moduleName: moduleName, offsets: offsets);
+        _WatchString(name, base_, length, moduleName: moduleName, offsets: offsets);
     }
 
     public void WatchString(string name, ReadStringType readStringType, string moduleName, object base_, params int[] offsets)
     {
-        WatchStringFull(name, base_, readStringType: readStringType, moduleName: moduleName, offsets: offsets);
+        _WatchString(name, base_, readStringType: readStringType, moduleName: moduleName, offsets: offsets);
     }
 
     public void WatchString(string name, int length, ReadStringType readStringType, string moduleName, object base_, params int[] offsets)
     {
-        WatchStringFull(name, base_, length, readStringType, moduleName, offsets);
+        _WatchString(name, base_, length, readStringType, moduleName, offsets);
     }
 
     public void WatchString(string name, Module module, object base_, params int[] offsets)
     {
-        WatchStringFull(name, base_, moduleName: module.Name, offsets: offsets);
+        _WatchString(name, base_, moduleName: module.Name, offsets: offsets);
     }
 
     public void WatchString(string name, int length, Module module, object base_, params int[] offsets)
     {
-        WatchStringFull(name, base_, length, moduleName: module.Name, offsets: offsets);
+        _WatchString(name, base_, length, moduleName: module.Name, offsets: offsets);
     }
 
     public void WatchString(string name, ReadStringType readStringType, Module module, object base_, params int[] offsets)
     {
-        WatchStringFull(name, base_, readStringType :readStringType, moduleName: module.Name, offsets: offsets);
+        _WatchString(name, base_, readStringType :readStringType, moduleName: module.Name, offsets: offsets);
     }
 
     public void WatchString(string name, int length, ReadStringType readStringType, Module module, object base_, params int[] offsets)
     {
-        WatchStringFull(name, base_, length, readStringType, module.Name, offsets);
+        _WatchString(name, base_, length, readStringType, module.Name, offsets);
     }
 
-    public void WatchStringFull(string name, object base_, int length = 128, ReadStringType readStringType = ReadStringType.AutoDetect,
+    public void WatchString(string name, object base_, int length = 128, ReadStringType readStringType = ReadStringType.AutoDetect,
+        string moduleName = null, params int[] offsets)
+    {
+        _WatchString(name, base_, length, readStringType , moduleName, offsets);
+    }
+
+    private void _WatchString(string name, object base_, int length = 128, ReadStringType readStringType = ReadStringType.AutoDetect,
         string moduleName = null, params int[] offsets)
     {
         try
