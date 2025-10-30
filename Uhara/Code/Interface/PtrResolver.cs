44fa3948e6e427d5433e1b4024db83c106ed6042
ru-mii <rumiireal@gmail.com>
2025-10-30 01:35:15 +0000

    Add WatchUnityString overloads to PtrResolver
    Introduced multiple overloads for the WatchUnityString method in PtrResolver to support various parameter combinations, improving flexibility for string watching in Unity. Added a private helper method to centralize logic and updated usage to handle different base pointer and module scenarios.

--- Uhara/Code/Interface/PtrResolver.cs
+++ Uhara/Code/Interface/PtrResolver.cs
@@ -147,4 +147,102 @@ public class PtrResolver : MainShared
         }
         catch { }
     }
+
+    // ---
+    public void WatchUnityString(string name, (IntPtr base_, int[] offsets) offsets)
+    {
+        _WatchUnityString(name, offsets.base_, offsets: offsets.offsets);
+    }
+
+    public void WatchUnityString(string name, object base_, params int[] offsets)
+    {
+        _WatchUnityString(name, base_, offsets: offsets);
+    }
+
+    public void WatchUnityString(string name, int length, object base_, params int[] offsets)
+    {
+        _WatchUnityString(name, base_, length: length, offsets: offsets);
+    }
+
+    public void WatchUnityString(string name, ReadStringType readStringType, object base_, params int[] offsets)
+    {
+        _WatchUnityString(name, base_, offsets: offsets);
+    }
+
+    public void WatchUnityString(string name, int length, ReadStringType readStringType, object base_, params int[] offsets)
+    {
+        _WatchUnityString(name, base_, length, offsets: offsets);
+    }
+
+    public void WatchUnityString(string name, string moduleName, object base_, params int[] offsets)
+    {
+        _WatchUnityString(name, base_, moduleName: moduleName, offsets: offsets);
+    }
+
+    public void WatchUnityString(string name, int length, string moduleName, object base_, params int[] offsets)
+    {
+        _WatchUnityString(name, base_, length, moduleName: moduleName, offsets: offsets);
+    }
+
+    public void WatchUnityString(string name, ReadStringType readStringType, string moduleName, object base_, params int[] offsets)
+    {
+        _WatchUnityString(name, base_, readStringType: readStringType, moduleName: moduleName, offsets: offsets);
+    }
+
+    public void WatchUnityString(string name, int length, ReadStringType readStringType, string moduleName, object base_, params int[] offsets)
+    {
+        _WatchUnityString(name, base_, length, readStringType, moduleName, offsets);
+    }
+
+    public void WatchUnityString(string name, Module module, object base_, params int[] offsets)
+    {
+        _WatchUnityString(name, base_, moduleName: module.Name, offsets: offsets);
+    }
+
+    public void WatchUnityString(string name, int length, Module module, object base_, params int[] offsets)
+    {
+        _WatchUnityString(name, base_, length, moduleName: module.Name, offsets: offsets);
+    }
+
+    public void WatchUnityString(string name, ReadStringType readStringType, Module module, object base_, params int[] offsets)
+    {
+        _WatchUnityString(name, base_, readStringType: readStringType, moduleName: module.Name, offsets: offsets);
+    }
+
+    public void WatchUnityString(string name, int length, ReadStringType readStringType, Module module, object base_, params int[] offsets)
+    {
+        _WatchUnityString(name, base_, length, readStringType, module.Name, offsets);
+    }
+
+    public void WatchUnityString(string name, object base_, int length = 128, ReadStringType readStringType = ReadStringType.AutoDetect,
+        string moduleName = null, params int[] offsets)
+    {
+        _WatchUnityString(name, base_, length, readStringType, moduleName, offsets);
+    }
+
+    private void _WatchUnityString(string name, object base_, int length = 128, ReadStringType readStringType = ReadStringType.UTF16,
+        string moduleName = null, params int[] offsets)
+    {
+        try
+        {
+            List<int> _offsets = offsets.ToList();
+            _offsets.Add(0x14);
+            offsets = _offsets.ToArray();
+
+            DeepPointer deepPointer;
+            if (base_.GetType() == typeof(int))
+            {
+                if (string.IsNullOrEmpty(moduleName)) deepPointer = new DeepPointer((int)base_, offsets);
+                else deepPointer = new DeepPointer(moduleName, (int)base_, offsets);
+            }
+            else deepPointer = new DeepPointer((IntPtr)base_, offsets);
+
+            StringWatcher stringWatcher = new StringWatcher(deepPointer, 128);
+            stringWatcher.Name = name;
+            StringWatchers.Add(stringWatcher);
+
+            current[name] = null;
+        }
+        catch { }
+    }
 }
