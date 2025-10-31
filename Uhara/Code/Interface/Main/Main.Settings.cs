17c505d39d733c4e5336a425bfac7af13034e929
ru-mii <rumiireal@gmail.com>
2025-10-31 14:18:57 +0000

    Enhance ScriptSettings creation with custom order
    Added CreateCustom and updated Create methods in ScriptSettings to support custom column ordering and handle both 3 and 4 column settings arrays. Improved error handling and flexibility for adding settings.

--- Uhara/Code/Interface/Main/Main.Settings.cs
+++ Uhara/Code/Interface/Main/Main.Settings.cs
@@ -10,23 +10,54 @@ public partial class Main : MainShared
 {
 	public class ScriptSettings
 	{
-		public void Create(dynamic[,] settings)
+        public void CreateCustom(dynamic[,] settings, params int[] order)
+        {
+            try
+            {
+                Create(settings, order);
+            }
+            catch { }
+        }
+
+        public void Create(dynamic[,] settings, params int[] order)
 		{
 			try
 			{
 				int rows = settings.GetLength(0);
 				int cols = settings.GetLength(1);
 
-				if (cols != 4)
+				if (cols != 4 && cols != 3)
 				{
-					TUtils.Print("Settings could not be parsed!");
+					TUtils.Print("Settings could not be parsed");
 					return;
 				}
 
-				for (int i = 0; i < rows; i++)
+                int[] _order = new int[] { 1, 2, 3, 4 };
+                if (order.Length != 0)
 				{
-                    MainShared._ScriptSettings.AddSetting(settings[i, 0], settings[i, 1], settings[i, 2], settings[i, 3]);
+					if (order.Length != cols)
+					{
+						TUtils.Print("Order count did not match the settings columns");
+						return;
+					}
+
+					_order = order;
 				}
+
+                if (cols == 3)
+				{
+                    for (int i = 0; i < rows; i++)
+					{
+                        MainShared._ScriptSettings.AddSetting(settings[i, _order[0] - 1], settings[i, _order[1] - 1], settings[i, _order[2] - 1], null);
+                    }
+                }
+				else if (cols == 4)
+				{
+                    for (int i = 0; i < rows; i++)
+                    {
+                        MainShared._ScriptSettings.AddSetting(settings[i, _order[0]], settings[i, _order[1]], settings[i, _order[2]], settings[i, _order[3]]);
+                    }
+                }
 			}
 			catch { }
 		}
